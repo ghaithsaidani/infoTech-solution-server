f1cda410cf69eb7ca3e57d9432b94089be408401
ghaith <ghaith.saidani@confledis.com>
2023-11-23 00:19:09 +0100

    server-starter

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
